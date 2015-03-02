d4e0f2715b34fbdd415bbca59201df8632c79624
Damen Turnbull <info@fando.com.au>
2015-03-02 23:42:00 +1100

    Use of subject syntax

--- spec/lib/beer_spec.rb
+++ spec/lib/beer_spec.rb
@@ -12,21 +12,11 @@ describe Beer do
 			expect(beer.get_name).to eq("Pale Ale")
 		end
 	end
+
+	describe "#get_abv" do
+		subject(:abv) { Beer.new("Pale Ale", "4.5%").get_abv }
+		it do
+			is_expected.to eq("4.5%")
+		end
+	end
 end
-
-# Other examples
-# describe '#ingredients' do
-#   it 'should contain barley and water' do
-#       beer = Beer.new
-#       expect(beer.ingredients).to include "Barley"
-#       expect(beer.ingredients).to include "Water"
-#   end
-# end
-
-# describe '#ingredients' do
-#   subject { Beer.new.ingredients }
-#   it do
-#       is_expected.to include "Barley"
-#       is_expected.to include "Water"
-#   end
-# end
